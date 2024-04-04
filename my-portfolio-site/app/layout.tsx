6299dc8bddc92a7fee39a72df2bdaa5fa2a74be5
benabbott <ben.abbott1717@gmail.com>
2024-04-04 18:39:26 +0100

    meta data changed

--- my-portfolio-site/app/layout.tsx
+++ my-portfolio-site/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Home",
-  description: "Welcome to My Website!",
+  description: "Welcome to my portfolio",
 };
 
 export default function RootLayout({
