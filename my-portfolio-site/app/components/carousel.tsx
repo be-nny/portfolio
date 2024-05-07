fd9461c77d4535096a04a0dddb1823b5791ad589
benabbott <ben.abbott1717@gmail.com>
2024-05-07 16:13:58 +0100

    home page css

--- my-portfolio-site/app/components/carousel.tsx
+++ my-portfolio-site/app/components/carousel.tsx
@@ -1,32 +1,32 @@
-import { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import Image1 from '@/public/images/iSABEL logo (dark).png'
-export const ControlledCarousel = () => {
-    return (
-        <Carousel data-bs-theme="light">
-            <Carousel.Item>
-                <img src={'/images/isabel_poster_1.png'} width={'50%'} />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={Image1} />
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={Image1} />
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
-    );
-}
+// import { useState } from 'react';
+// import Carousel from 'react-bootstrap/Carousel';
+// import Image1 from '@/public/images/iSABEL logo (dark).png'
+// export const ControlledCarousel = () => {
+//     return (
+//         <Carousel data-bs-theme="light">
+//             <Carousel.Item>
+//                 <img src={'/images/isabel_poster_1.png'} width={'50%'} />
+//                 <Carousel.Caption>
+//                     <h3>First slide label</h3>
+//                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//                 </Carousel.Caption>
+//             </Carousel.Item>
+//             <Carousel.Item>
+//                 <img src={'/images/isabel_poster_1.png'} width={'50%'} />
+//                 <Carousel.Caption>
+//                     <h3>Second slide label</h3>
+//                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//                 </Carousel.Caption>
+//             </Carousel.Item>
+//             <Carousel.Item>
+//                 <img src={'/images/isabel_poster_1.png'} width={'50%'} />
+//                 <Carousel.Caption>
+//                     <h3>Third slide label</h3>
+//                     <p>
+//                         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//                     </p>
+//                 </Carousel.Caption>
+//             </Carousel.Item>
+//         </Carousel>
+//     );
+// }
