a60c1ffd3fd58389e4e40920cf61511a856c5af5
benabbott <ben.abbott1717@gmail.com>
2024-05-13 13:38:25 +0100

    removed 3js

--- my-portfolio-site/app/page.tsx
+++ my-portfolio-site/app/page.tsx
@@ -14,6 +14,5 @@ export default function Home() {
             <LandingPage/>
         </div>
     </div>
-
     );
 }
--- my-portfolio-site/app/scripts/prismjsLoader.js
+++
@@ -1,6 +0,0 @@
-import 'prismjs';
-import 'prismjs/components/prism-markup-templating';
-import 'prismjs/components/prism-php';
-import 'prismjs/components/prism-json';
-import 'prismjs/components/prism-java';
-import 'prismjs/themes/prism-okaidia.css';
